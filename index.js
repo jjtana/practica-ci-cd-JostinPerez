845753ec12b26ad67a9c7a803ff25f4d480efad1
jjtana <151287087+jjtana@users.noreply.github.com>
2026-02-10 17:41:22 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("¡Hola Mundo desde mi práctica de Integración Continua!");
+console.log("DevOps ITLA - Febrero 2026 🚀");
+console.log("Todo funcionando correctamente con GitHub Actions.");
